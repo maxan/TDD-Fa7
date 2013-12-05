f25f5f0c5c065c6b618240048b52d9152835df9c
unknown <posgrad@FA7-LTRM10.7setlabs.local>
2013-12-04 22:09:28 -0300

    Exercício com teste spy sobre objetos compostos.

--- exemplo_mock3.rb
+++ exemplo_mock3.rb
@@ -1,23 +1,41 @@
+# encoding: utf-8
+
+# Concluir este teste.
+
 class Inscricao
-	def initialize (candicato)
+	def initialize (candicato, logger = nil)
 		@candicato = candicato
+		@logger = logger
 	end
 
 	def gerar
-		"Inscricao de #{@candicato.nome}"
+		@logger.log("Inscrição de #{@candicato.nome}") unless @logger.nil?
+		"Inscrição de #{@candicato.nome}"
 	end
 end
 
 describe Inscricao do
 	context "#gerar" do
-		it "utiliza o nome do candidato no cabecalho" do
+		it "utiliza o nome do candidato no cabecalho é" do
 
 		candidato = double("candidato")
 		candidato.stub(:nome).and_return("Luiz")
 
 		inscricao = Inscricao.new(candidato)
-		expect(inscricao.gerar).to eql "Inscricao de Luiz"
+		expect(inscricao.gerar).to eql "Inscrição de Luiz"
 
+		end
+		
+		it "cria um log quando o gerar é chamado" do
+			candidato = stub("candicato")
+			candidato.stub(:nome).and_return("Luiz")
+
+			logger = double("logger")
+			inscricao = Inscricao.new(candidato, logger)
+
+			logger.should_receive(:log).with("Inscrição de Luiz")
+		
+			inscricao.gerar
 		end
 	end
 end
